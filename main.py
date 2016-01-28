3b01ce78885af1ca0a779207abb513bed52ca847
a1628828 <a1628828@student.adelaide.edu.au>
2016-01-29 00:39:21 +1030

    Add link file handler and controls. Needs nicer formatting and moderator check.

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import discord
 import asyncio
 import bnet
+import json
 
 import os
 
 client = discord.Client()
 
 PASSWORD = os.environ.get('EVOKEBOT_PASSWORD')
+
+LINK_FILE_NAME = 'links.txt'
+
+def readLinksFile():
+    out = json.load(open(LINK_FILE_NAME))
+    return out
+
+def addToLinks(tag, url):
+    data = readLinksFile()
+    data[tag] = url
+    json.dump(data, open(LINK_FILE_NAME, 'w'))
+
+def removeFromLinks(tag):
+    data = readLinksFile()
+    if tag in data:
+        del data[tag]
+        json.dump(data, open(LINK_FILE_NAME, 'w'))
 
 @client.event
 async def on_ready():
@@ -36,6 +54,19 @@ async def on_message(message):
         await client.send_message(message.channel, 'Sleeping for 60 seconds. Wake me up when Gorefiend is dead.')
         await asyncio.sleep(60)
         await client.send_message(message.channel, 'Done sleeping')
+
+    elif message.content.startswith('!links'):
+        args = message.content.split()
+        if len(args) == 1:
+            data = readLinksFile()
+            await client.send_message(message.channel, data)
+        else:
+            if args[1] == 'add':
+                addToLinks(args[2], args[3])
+            elif args[1] == 'delete':
+                removeFromLinks(args[2])
+            else:
+                print (links)
 
     elif message.author.name.startswith('Bobnamob'):
         if message.content.startswith('dumb'):
