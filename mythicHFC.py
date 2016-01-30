480af0eab2487d09246b1781b91514966c4da1e6
NadejM <merlinisawizard@gmail.com>
2016-01-30 12:15:38 +1000

    Create mythicHFC.py

---
+++ mythicHFC.py
@@ -0,0 +1,31 @@
+
+
+def mythicbossfights(fight, diff):
+    if (fight == 'HFA') and (diff =='Mythic'):
+        fightvid = 'https://www.youtube.com/watch?v=HTqO9jpGPfc'
+    if fight == 'IR' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=Kl-WlFkw3LI'
+    if fight == 'Kormrok' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=Hs6vs9rUQpc'
+    if fight == 'HHC' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=rqbpO1yJBZo'
+    if fight == 'Kilrogg' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=L-6ssVlz8Eg'
+    if fight == 'Gorefiend' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=-YbM8lEIvG4'
+    if fight == 'Iskar' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=xqHZ9N0UTUE'
+    if fight == 'Socrethar' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=O04dNj_wRyA'
+    if fight == 'Zakuun' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=AGuQ3ibEdKQ'
+    if fight == 'Xhul' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=3vDyv06DplA'
+    if fight == 'Velhari' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=cO6js7IxlmI'
+    if fight == 'Mannoroth' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=ErZBp8U1raQ'
+    if fight == 'Archimonde' and diff =='Mythic':
+        fightvid = 'https://www.youtube.com/watch?v=46Sd3xCpyzo'
+
+    return fightvid
